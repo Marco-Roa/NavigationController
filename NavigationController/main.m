8f2075f8265ac3ae030cefe4bd157006ea0a9c13
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-10-18 19:42:55 -0500

    Initial Commit

---
+++ NavigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavigationController
+//
+//  Created by Marco Antonio Roa De La Cruz on 10/18/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
